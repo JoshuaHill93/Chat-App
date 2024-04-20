92856ce4f640de25b4ddd52003761c7aa01901bf
joshua_hill <joshuah923@gmail.comc:/Users/joshu/OneDrive/Desktop/hacker/CSS-The-Hacker-Times-Part-1/CSS-The-Hacker-Times-Part-1/index.html>
2024-04-20 12:19:10 -0700

    chat app

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const connectDB = require("./config/db")
+//const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const cors = require("cors");
+const path = require('path')
+require('dotenv').config({path:path.resolve(__dirname,'./.env')})
+
+
+
+//dotenv.config();
+connectDB();
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+app.get("/",(req,res) => {
+    res.send("Hello Welcome to Chatopia")
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat",chatRoutes)
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log("Server Started on PORT ${PORT}"));
+
+
+
