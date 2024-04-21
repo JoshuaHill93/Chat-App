89b403642c59d00549d659318a5f7912a01d43f5
joshua_hill <joshuah923@gmail.comc:/Users/joshu/OneDrive/Desktop/hacker/CSS-The-Hacker-Times-Part-1/CSS-The-Hacker-Times-Part-1/index.html>
2024-04-20 18:31:24 -0700

    added profice and logout box

--- frontend/src/components/miscellaneous/ProfileModal.js
+++ frontend/src/components/miscellaneous/ProfileModal.js
@@ -1,67 +1,56 @@
 import { ViewIcon } from "@chakra-ui/icons";
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  Button,
-  useDisclosure,
-  IconButton,
-  Text,
-  Image,
-} from "@chakra-ui/react";
+import {  useDisclosure } from "@chakra-ui/hooks";
+import React from "react";
+import { IconButton } from "@chakra-ui/button";
 
+import { Modal, ModalOverlay, ModalContent, ModalHeader,  ModalFooter,  ModalBody,  ModalCloseButton, Button,  Text, Image,} from "@chakra-ui/react";
 const ProfileModal = ({ user, children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  return (
-    <>
-      {children ? (
-        <span onClick={onOpen}>{children}</span>
-      ) : (
-        <IconButton d={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
-      )}
-      <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
+
+return (
+<>
+{children ? (
+<span onClick={onOpen}>{children}</span>
+) : (
+  <IconButton d={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
+)}
+
+<Modal size="sm" isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
-        <ModalContent h="410px">
+        <ModalContent>
           <ModalHeader
-            fontSize="40px"
-            fontFamily="Work sans"
-            d="flex"
-            justifyContent="center"
+          fontSize="40px"
+          fontFamily="Work sans"
+          d="flex"
+          justifyContent="center"
           >
             {user.name}
-          </ModalHeader>
+            </ModalHeader>
           <ModalCloseButton />
-          <ModalBody
-            d="flex"
-            flexDir="column"
-            alignItems="center"
-            justifyContent="space-between"
-          >
+          <ModalBody>
             <Image
-              borderRadius="full"
-              boxSize="150px"
-              src={user.pic}
-              alt={user.name}
+            borderRadius="full"
+            boxSide="150"
+            src={user.pic}
+            alt={user.name}
             />
-            <Text
-              fontSize={{ base: "28px", md: "30px" }}
-              fontFamily="Work sans"
-            >
-              Email: {user.email}
-            </Text>
+            <Text fontSize={{ base: "28", md: "30px"}}
+                   fontFamily="Work sans"
+                   >
+                    Email: {user.email}
+                    </Text>
           </ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose}>Close</Button>
+            <ModalFooter>
+            <Button colorScheme='purple' mr={3} onClick={onClose}>
+              Close
+            </Button>
+       
           </ModalFooter>
         </ModalContent>
       </Modal>
-    </>
-  );
-};
 
-export default ProfileModal;
+</>
+);
+}
+export default ProfileModal
--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,9 +1,12 @@
 import { Box, Text } from "@chakra-ui/layout";
-import { Tooltip } from '@chakra-ui/react';
+import { Menu, MenuButton, Tooltip } from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon} from "@chakra-ui/icons";
 import { Button } from "@chakra-ui/button";
+import { Avatar } from "@chakra-ui/react";
 import React, { useState } from 'react';
-
-
+import { ChatState } from "../../Context/ChatProvider";
+import {  MenuItem, MenuList, MenuDivider, }  from "@chakra-ui/menu";
+import ProfileModal from "./ProfileModal";
 
 
 
@@ -13,6 +16,9 @@ const SideDrawer = () => {
     const [loading, setLoading] = useState(false);
     const [loadingChat, setLoadingChat] = useState();
 
+
+    const { user } = ChatState();
+
   return <>
   <Box
   d="flex"
@@ -21,16 +27,42 @@ const SideDrawer = () => {
   bg="white"
   w="100%"
   p="5px 10px 5px 10px"
-  borderWidth="5px"
+  borderWidth="1px"
   >
 <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
 <Button variant="ghost">
   <i class="fas fa-search"></i>
-  <Text d={{base: "none", md: "flex"}} px="4" >
+  <Text d={{base: "none", md: "flex"}} px="1" >
     Search Users
   </Text>
 </Button>
 </Tooltip>
+
+<Text fontSize="3xl" fontFamily="Work sans" textAlign="center">
+  Chatopia
+</Text>
+
+<div>
+  <Menu>
+    <MenuButton p={1}>
+      <BellIcon fontSize="2xl" m={1}/>
+    </MenuButton>
+      {/*<MenuList></MenuList>*/}
+  </Menu>
+
+<Menu>
+  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+  <Avatar  size="sm" cursor="pointer" name={user.name} src={user.pic} />
+  </MenuButton>
+<MenuList>
+  <ProfileModal user={user}>
+  <MenuItem>My Profile</MenuItem>
+  </ProfileModal>
+  <MenuDivider />
+  <MenuItem>Logout</MenuItem>
+</MenuList>
+</Menu>
+</div>
   </Box>
   </>
   };
